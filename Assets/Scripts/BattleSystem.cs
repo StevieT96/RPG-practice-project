a94561e20987a9e284dc5afbfc6855b3b85629b2
StevieT96 <s.tran1996@gmail.com>
2023-12-15 16:00:17 +1100

    Added additional assets to battle scene

--- Assets/Scripts/BattleSystem.cs
+++ Assets/Scripts/BattleSystem.cs
@@ -1,3 +1,4 @@
+using Assets.Scripts;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,7 +7,25 @@ namespace Assets.Scripts
 {
     public enum BattleState { Begin, PlayerTurn, EnemyTurn, Victory, Defeat}
 }
-public class BattleSystem
+public class BattleSystem : MonoBehaviour
 {
+    public GameObject playerPrefab;
+    public GameObject enemyPrefab;
 
-} 
+    public Transform EnemyArea;
+    
+
+    public BattleState state;
+
+    void Start()
+    {
+        state = BattleState.Begin;
+        SetupBattle();
+       
+    }
+    void SetupBattle()
+    {
+       // Instantiate(playerPrefab,); //
+        Instantiate(enemyPrefab, EnemyArea);
+    }
+}
